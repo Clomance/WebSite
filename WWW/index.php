7211e6111b104fd9c7a915ae14a84eb5cd4b8ee5
Clone <hisoskar@gmail.com>
2020-09-22 23:35:18 +0300

    Creating a repository.

---
+++ WWW/index.php
@@ -0,0 +1,11 @@
+<p> Дата и время:
+<p>
+<?php
+    $d=date("d.m.Y H:i");
+    echo($d);
+?>
+<ul id="nav">
+    <li><a href="lab_1_1.php">lab_1_1</a>
+    <li><a href="lab_1_2.php">lab_1_2</a>
+    <li><a href="lab_1_3.php">lab_1_3</a>
+</ul>
