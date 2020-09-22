70024c9f968fadcd3c8f3f9d3db00d709815cd4d
Clone <hisoskar@gmail.com>
2020-09-23 02:54:55 +0300

    Updated

--- index.php
+++ index.php
@@ -1,6 +1,55 @@
-<p> Список студентов ПИ-318
+<style>
+    header{
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        background-color: #ffffff;
+        padding-top: 10px;
+        padding-bottom: 10px;
+        padding-left: 10px;
+        padding-right: 10px;
+        background: rgba(242, 243, 244, 0.6);
+        align-items: center;
+    }
 
-<ul id="nav">
-    <li><a href="https://pialikin.herokuapp.com">Аликин В.П.</a>
-    <li><a href="Khismatullin">Хисматуллин О.М.</a>
-</ul>
+    header text{
+        position:inherit;
+
+        margin-left:10px;
+
+        font-size: 72px;
+    }
+
+    img{
+        position:relative;
+        border-radius:8px;
+        border:solid;
+        height:110px;
+        width:110px;
+    }
+
+    body{
+        background-color:lightblue;
+        margin:0;
+        padding:0;
+    }
+</style>
+
+<html>
+    <header>
+        <img src="https://sun1.ufanet.userapi.com/svOT89nbDylVnfW8jScgP5LexI5KVoqa7Qhrow/o_rkxsJDupg.jpg">
+        <text>ПИ-318 - лучшая группа!</text>
+    </header>
+
+    <ul id="nav">
+        <li><a href="https://pialikin.herokuapp.com">Аликин В.П.</a>
+        
+        <li><a href="https://crocodev.herokuapp.com/">Разяпов Э.В.</a>
+
+        <li><a href="Khismatullin">Хисматуллин О.М.</a>
+    </ul>
+</html>
+
+
+
+
