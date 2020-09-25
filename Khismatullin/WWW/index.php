153f4135a1a12c0c4113a52e762bfc192e991b12
Clone <hisoskar@gmail.com>
2020-09-25 21:34:07 +0300

    Updated

--- Khismatullin/WWW/index.php
+++ Khismatullin/WWW/index.php
@@ -3,9 +3,11 @@
 <?php
     $d=date("d.m.Y H:i");
     echo($d);
-?>
+?>  
 <ul id="nav">
     <li><a href="lab_1_1.php">lab_1_1</a>
     <li><a href="lab_1_2.php">lab_1_2</a>
     <li><a href="lab_1_3.php">lab_1_3</a>
+    <li><a href="lab_1_5.php">lab_1_5</a>
+    <li><a href="lab_1_5_2.php">lab_1_5_2</a>
 </ul>
--- Khismatullin/WWW/lab_1_3.php
+++ Khismatullin/WWW/lab_1_3.php
@@ -2,10 +2,7 @@
 <p>
 <?php
     define('NUM_E',2.71828);
-
     $num_e1=NUM_E;
-
     settype($num_e1,'boolean');
-
-    print('$num_e1 = '.$num_e1.' - '.gettype(NUM_E));
+    print('$num_e1 = '.$num_e1.' - '.gettype($num_e1));
 ?>
