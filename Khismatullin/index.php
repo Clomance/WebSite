dd8dd1ff4d5eee59e836c2751b6bb01facfcf0e1
Clomance <Hisoskar@gmail.com>
2021-03-06 23:02:14 +0300

    Removed old, added new.

--- Khismatullin/index.php
+++ Khismatullin/index.php
@@ -1,9 +1,14 @@
-<p> Список лаб
+<html>
+    <p> Список лаб </p>
 
-<ul id="nav">
-    <li><a href="WWW">Лаба 1</a>
-    <li><a href="WWWW">Лаба 2</a>
-    <li><a href="WWWWW">Лаба 3</a>
-    <li><a href="WWWWWW">Лаба 4</a>
-    <li><a href="WWWWWWW">Лаба 5</a>
-</ul>
+    <ul id="nav">
+        <li><a href="WWW">Лаба 1</a>
+        <li><a href="WWWW">Лаба 2</a>
+        <li><a href="WWWWW">Лаба 3</a>
+        <li><a href="WWWWWW">Лаба 4</a>
+        <li><a href="WWWWWWW">Лаба 5</a>
+
+        <li><a href="old">Интернет-программирование</a>
+    </ul>
+
+</html>
---
+++ Khismatullin/old/index.php
@@ -0,0 +1,9 @@
+<p> Список лаб
+
+<ul id="nav">
+    <li><a href="WWW">Лаба 1</a>
+    <li><a href="WWWW">Лаба 2</a>
+    <li><a href="WWWWW">Лаба 3</a>
+    <li><a href="WWWWWW">Лаба 4</a>
+    <li><a href="WWWWWWW">Лаба 5</a>
+</ul>
